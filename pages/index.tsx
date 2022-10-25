342b6ff7480f3337268a118efb19cd785828f8c6
navidgoalpure <navid.goalpure@gmail.com>
2022-10-25 16:14:26 +0330

    1:16 time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,56 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Header from '../components/Header';
+import { sanityClient, urlFor } from '../sanity';
+import { Post } from '../typings';
 
-const Home: NextPage = () => {
+interface Props {
+  posts: Post[];
+}
+const Home: NextPage<Props> = ({ posts }) => {
+  console.log('navid posts=', posts);
   return (
-    <div className='flex min-h-screen flex-col items-center justify-center py-2'>
+    <div className='max-w-7xl mx-auto'>
       <Head>
         <title>Medium Blog</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <h1>This is Medium </h1>
+      <Header />
+      <div className='flex justify-between items-center bg-indigo-400 border-y py-10 lg:py-0'>
+        <div className='px-10 space-y-5'>
+          <h1 className='text-6xl max-w-xl font-serif'>Stay curious.</h1>
+          <h2>
+            Discover stories, thinking, and expertise from writers on any topic.
+          </h2>
+        </div>
+        <img
+          src='https://seeklogo.com/images/M/medium-logo-93CDCF6451-seeklogo.com.png'
+          className='hidden md:inline-flex h-32 lg:h-full'
+        />
+      </div>
+      {/* posts */}
     </div>
   );
 };
+export const getServerSideProps = async () => {
+  const query = `*[_type=='post']{
+ _id,
+  title,
+  description,
+  author -> {
+    name,
+    image},
+  description,
+  mainImage,
+  slug
+}`;
 
+  const posts = await sanityClient.fetch(query);
+  return {
+    props: {
+      posts,
+    },
+  };
+};
 export default Home;
---
+++ sanity.js
@@ -0,0 +1,46 @@
+// lib/sanity.js
+import {
+  createPreviewSubscriptionHook,
+  createCurrentUserHook,
+  createClient,
+} from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
+
+export const config = {
+  /**
+   * Find your project ID and dataset in `sanity.json` in your studio project.
+   * These are considered “public”, but you can use environment variables
+   * if you want differ between local dev and production.
+   *
+   * https://nextjs.org/docs/basic-features/environment-variables
+   **/
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: '2021-10-21', // Learn more: https://www.sanity.io/docs/api-versioning
+  /**
+   * Set useCdn to `false` if your application require the freshest possible
+   * data always (potentially slightly slower and a bit more expensive).
+   * Authenticated request (like preview) will always bypass the CDN
+   **/
+  useCdn: process.env.NODE_ENV === 'production',
+
+  /**
+   * OPTIONAL config to enable authentication with custom token
+   * You might need this if you host the preview on a different url than Sanity Studio
+   */
+  //   token: '<sanity access token>',
+  //   EventSource: /* provide your own event source implementation. Required in browsers to support the above token parameter. */
+};
+
+// // Set up the live preview subscription hook
+// export const usePreviewSubscription = createPreviewSubscriptionHook(config);
+
+// // Helper function for using the current logged in user account
+// export const useCurrentUser = createCurrentUserHook(config);
+
+export const sanityClient = createClient(config);
+/**
+ * Set up a helper function for generating Image URLs with only the asset reference data in your documents.
+ * Read more: https://www.sanity.io/docs/image-url
+ **/
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);

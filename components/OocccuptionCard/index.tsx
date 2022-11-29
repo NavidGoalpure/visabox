71d7794389d0417ee3ee576e144372369429396a
Farzam Khoddam <farzamfara85@gmail.com>
2022-11-29 16:43:57 +0330

    minor improvement

--- components/OocccuptionCard/index.tsx
+++ components/OocccuptionCard/index.tsx
@@ -1,12 +1,13 @@
-import { ComponentTitleStyle } from 'components/Layouts/StyledComponents';
-import { useLocale } from 'hooks/useLocale';
-import { MultiLanguageText } from 'interfaces';
-import { Slug } from 'interfaces/Fields';
-import Link from 'next/link';
-import styled, { css } from 'styled-components/macro';
-import theme from 'styled-theming';
-import { componentTextColor, componentTheme } from 'styles/Theme';
-import { Headline6Style } from 'styles/Typo';
+import { ComponentTitleStyle } from "components/Layouts/StyledComponents";
+import { useLocale } from "hooks/useLocale";
+import { MultiLanguageText } from "interfaces";
+import { Slug } from "interfaces/Fields";
+import Link from "next/link";
+import styled, { css } from "styled-components/macro";
+import theme from "styled-theming";
+import { componentTextColor, componentTheme } from "styles/Theme";
+import { Headline6Style } from "styles/Typo";
+import { IoIosArrowDown } from "react-icons/io";
 
 interface Props {
   code?: number;
@@ -26,11 +27,14 @@ function OccupationCard({ code, title, description, slug }: Props) {
       <Container>
         {/* <Code>{code}</Code> */}
         <Code>121111</Code>
-
         {/* <Title>{title?.[locale]}</Title> */}
         <Title>Grain, Oilseed or Pasture Grower / Field Crop Grower</Title>
 
         <Description>{description?.[locale]}</Description>
+        <PopupContainer>
+          {" "}
+          <Arrow />{" "}
+        </PopupContainer>
       </Container>
     </Link>
   );
@@ -53,11 +57,13 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 20rem;
-  padding-bottom: 3.75rem;
+  width: 23rem;
   border-radius: 15px;
-  padding-top: 1.5rem;
+  padding: 1.5rem 1rem 3.75rem 1rem;
   cursor: pointer;
+  max-width: 31%;
+  position: relative;
+  overflow: hidden;
 `;
 
 const Code = styled.h3`
@@ -67,7 +73,11 @@ const Code = styled.h3`
   padding: 0.5rem;
   align-items: center;
   border-radius: 55px;
+  ${Container}:hover & {
+    z-index: 4;
+  }
 `;
+
 const Title = styled.h2`
   ${ComponentTitleStyle}
 `;
@@ -76,4 +86,33 @@ const Description = styled.p`
   ${componentTextColor}
   text-align: center;
   overflow: hidden;
+`;
+const PopupContainer = styled.div`
+  position: absolute;
+  top: 90%;
+  background-color: var(--color-gray9);
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem 1rem 3.75rem 1rem;
+  cursor: pointer;
+  transition: all 0.5s ease;
+  ${Container}:hover & {
+    top: 0;
+  }
+`;
+const Arrow = styled(IoIosArrowDown)`
+  position: absolute;
+  top: -1rem;
+  left: 50%;
+  transform: translateX(-50%) rotate(180deg);
+  fill: white;
+  background-color: var(--color-primary4);
+  width: 2.5rem;
+  height: auto;
+  border-radius: 50px;
+  padding: 0.5rem;
+  transition: all 0.4s ease;
+  ${Container}:hover & {
+    opacity: 0;
+  }
 `;

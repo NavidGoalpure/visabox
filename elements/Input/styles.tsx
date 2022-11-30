be50b909ccfc6f53284464afbd47336738ea9a11
navidgoalpure <navid.goalpure@gmail.com>
2022-11-30 11:58:57 +0330

    fix ui bug

--- elements/Input/styles.tsx
+++ elements/Input/styles.tsx
@@ -12,7 +12,7 @@ const Container = styled.div`
 `;
 export const inputContainerTheme = theme('mode', {
   light: css`
-    background: var(--color-gray7);
+    background: var(--color-gray13);
     border: 1px solid var(--color-gray10);
   `,
   dark: css`

67c33acd0fe8af4124c5f7b04d3ba5d3ac1d1b68
Farzam Khoddam <farzamfara85@gmail.com>
2022-12-07 12:57:11 +0330

    minor occupations [slug] improvement

--- PagesComponents/Occupations/Slug/index.tsx
+++ PagesComponents/Occupations/Slug/index.tsx
@@ -1,35 +1,61 @@
-import TooltipTag from 'elements/TooltipTag';
-import { Occupation } from 'interfaces/Documents/Occupation';
-import React from 'react';
-import ToggleTag from 'elements/ToggleTag';
-import styled from 'styled-components';
-import { SidebarPage } from './SideBar';
-import { PageTitleStyle } from 'Styles/Theme/Page';
+import TooltipTag from "elements/TooltipTag";
+import { Occupation } from "interfaces/Documents/Occupation";
+import React from "react";
+import ToggleTag from "elements/ToggleTag";
+import styled from "styled-components";
+import { SidebarPage } from "./SideBar";
+import { PageTitleStyle } from "Styles/Theme/Page";
+import { useDynamicTranslation } from "hooks/useDynamicTraslation";
 
 interface Props {
   occupation: Occupation;
 }
 const Content: React.FC<Props> = ({ occupation }) => {
+  const { dt } = useDynamicTranslation();
   return (
-    <>
-      <Title>data</Title>
-      <TooltipTag
-        content={
-          occupation?.anzsco_section?.priority_list
-            ? occupation?.anzsco_section?.priority_list[0]?.future_demend || ''
-            : ''
-        }
-        popupContent={'occupation.'}
-      />
-      <ToggleTag content={'190'} isOn={true} />
-      <ToggleTag content={'189'} isOn={false} />
+    <Container>
+      <TitleContainer>
+        <Title>
+          {occupation?.code}: {dt(occupation?.title)}
+        </Title>
+        <TooltipTag
+          content={
+            occupation?.anzsco_section?.priority_list
+              ? occupation?.anzsco_section?.priority_list[0]?.future_demend ||
+                ""
+              : ""
+          }
+          popupContent={"occupation."}
+        />
+      </TitleContainer>
+      <ToggleContainer>
+      <ToggleTag content={"190"} isOn={true} />
+      <ToggleTag content={"189"} isOn={false} />
+      </ToggleContainer>
       <SidebarPage occupation={occupation} />
-    </>
+    </Container>
   );
 };
 
 export default Content;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
 
 const Title = styled.h1`
-  ${PageTitleStyle}
+  ${PageTitleStyle};
+  margin: 0;
 `;
+const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+`;
+const ToggleContainer = styled.div`
+display:flex;
+
+`

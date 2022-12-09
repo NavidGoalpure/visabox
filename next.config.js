bba385412f6306229992ef092900d27a1363eb0c
navidgoalpure <navid.goalpure@gmail.com>
2022-12-09 15:33:27 +0330

    improve locale

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withImages = require('next-images');
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', 'fa'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
 };
 
 module.exports = withImages(config);
---
+++ pages/occupations/index.tsx
@@ -0,0 +1,42 @@
+import type { GetServerSideProps, NextPage } from 'next';
+import Head from 'next/head';
+import {
+  LanguageKeys,
+  componentStatements,
+} from 'PagesComponents/Occupations/const';
+import { useStaticTranslation } from 'hooks/useStaticTraslation';
+import Content from 'PagesComponents/Occupations';
+import PageLayout from 'components/Layouts/PageContainer';
+import { sanityClient } from 'sanity';
+import { dehydrate, QueryClient } from 'react-query';
+import { getListQuery } from 'PagesComponents/Occupations/utils';
+import { OccupationsQueryKeys } from 'utils/query';
+
+const OccupationList: NextPage = () => {
+  const { t } = useStaticTranslation(componentStatements);
+
+  return (
+    <PageLayout>
+      <Head>
+        <title>{t(LanguageKeys.PageTitle)}</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Content />
+    </PageLayout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  //
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(OccupationsQueryKeys.list({}), () =>
+    sanityClient.fetch(getListQuery({ searchCondition: '' }))
+  );
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export default OccupationList;

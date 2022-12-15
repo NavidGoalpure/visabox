ad5d43a3bad5f9e4573c41122d03d63644eb1ad2
navidgoalpure <navid.goalpure@gmail.com>
2022-12-15 15:34:54 +0330

    improve theme

--- Styles/Theme/Component.ts
+++ Styles/Theme/Component.ts
@@ -3,6 +3,14 @@ import theme from 'styled-theming';
 import { Headline5Style, Headline6Style, Headline7Style } from 'Styles/Typo';
 import { directionStyles } from '.';
 
+export const componentBorderColor = theme('mode', {
+  light: css`
+    border-color: var(--color-gray11);
+  `,
+  dark: css`
+    border-color: var(--color-gray9);
+  `,
+});
 //////
 export const componentTitleColor = theme('mode', {
   light: css`
@@ -50,6 +58,7 @@ export const componentTextStyle = css`
   ${Headline7Style}
   margin-bottom: 2rem;
 `;
+
 ///////////
 
 export const componentHeaderBackground = theme('mode', {
@@ -64,6 +73,8 @@ export const componentHeaderTheme = css`
   ${componentHeaderBackground}
   padding: 1rem 2rem;
   border-radius: 15px 15px 0 0;
+  border-bottom: 1px solid;
+  ${componentBorderColor}
 `;
 
 ///////////
@@ -77,14 +88,17 @@ export const componentBodyBackground = theme('mode', {
 });
 export const componentBodyTheme = css`
   ${componentBodyBackground}
-  padding: 0 2rem;
+  padding: 1rem 2rem;
+  border-radius: 0 0 15px 15px;
 `;
 //////////
 
 export const componentTheme = css`
+  ${componentBodyBackground}
   ${directionStyles}
-  border: 1px solid var(--color-gray9);
   border-radius: 15px;
+  border: 1px solid;
+  ${componentBorderColor}
 
   header {
     ${componentHeaderTheme}

fe79d9c0abbe3869ce34ef0c945b2566c18171e6
navidgoalpure <navid.goalpure@gmail.com>
2022-12-18 16:40:06 +0330

    improve

--- PagesComponents/Occupations/Slug/BacklogTab/noData.tsx
+++ PagesComponents/Occupations/Slug/BacklogTab/noData.tsx
@@ -1,11 +1,22 @@
 import { useStaticTranslation } from 'Hooks/useStaticTraslation';
 import React from 'react';
+import styled from 'styled-components/macro';
 import { componentStatements, LanguageKeys } from './const';
 
 const NoData = () => {
   const { t } = useStaticTranslation(componentStatements);
 
-  return <p>{t(LanguageKeys.NoData)}</p>;
+  return (
+    <Container>
+      <p>{t(LanguageKeys.NoData)}</p>;
+    </Container>
+  );
 };
 
 export default NoData;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+`;

1f6aec0c055d8ab1db3ca183a5d41d81f2814d0f
navidgoalpure <navid.goalpure@gmail.com>
2023-01-07 00:46:21 +0330

    try to fix bug

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,26 +1,10 @@
-import { deviceMin } from 'Consts/device';
-import styled from 'styled-components';
+import useDevice from 'Hooks/useDevice';
 import DesktopFooter from './DesktopFooter';
 import MobileFooter from './MobileFooter';
 
 const SmartFooter = () => {
-  return (
-    <>
-      <StyledMobileFooter />
-      <StyledDesktopFooter />
-    </>
-  );
+  const { isTablet, isMobile } = useDevice();
+  if (isTablet || isMobile) return <MobileFooter />;
+  return <DesktopFooter />;
 };
 export default SmartFooter;
-
-const StyledMobileFooter = styled(MobileFooter)`
-  @media${deviceMin.laptopXS} {
-    display: none;
-  }
-`;
-const StyledDesktopFooter = styled(DesktopFooter)`
-  display: none;
-  @media${deviceMin.laptopXS} {
-    display: initial;
-  }
-`;

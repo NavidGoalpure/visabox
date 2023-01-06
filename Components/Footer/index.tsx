3ee5a0dbf2120ca2c35399fefdf03a47d767d383
navidgoalpure <navid.goalpure@gmail.com>
2023-01-07 00:29:34 +0330

    change smartFooter system

--- Components/Footer/index.tsx
+++ Components/Footer/index.tsx
@@ -1,10 +1,25 @@
+import { deviceMin } from 'Consts/device';
 import useDevice from 'Hooks/useDevice';
+import styled from 'styled-components';
 import DesktopFooter from './DesktopFooter';
 import MobileFooter from './MobileFooter';
 
 const SmartFooter = () => {
-  const { isTablet, isMobile } = useDevice();
-  if (isTablet || isMobile) return <MobileFooter />;
-  return <DesktopFooter />;
+  <>
+    <StyledMobileFooter />
+    <StyledDesktopFooter />
+  </>;
 };
 export default SmartFooter;
+
+const StyledMobileFooter = styled(MobileFooter)`
+  @media${deviceMin.laptopXS} {
+    display: none;
+  }
+`;
+const StyledDesktopFooter = styled(MobileFooter)`
+  display: none;
+  @media${deviceMin.laptopXS} {
+    display: initial;
+  }
+`;

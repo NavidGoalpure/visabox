9ced905d9231723db56f040f90fe335c14d1657f
navidgoalpure <navid.goalpure@gmail.com>
2023-01-07 15:07:37 +0330

    remve useless

--- Queries/sitemap.ts
+++ Queries/sitemap.ts
@@ -1,7 +1,5 @@
 import { sanityClient } from 'Utils/sanity';
-
 import { Occupation } from 'Interfaces/Documents/occupation';
-import { OCCUPATION_PER_PAGE } from 'PagesComponents/Occupations/const';
 
 //////////////////////////
 ////////////////////////////

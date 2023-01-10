7a5141565b26739b329604e8d9a0a46480fdb87f
navidgoalpure <navid.goalpure@gmail.com>
2023-01-10 19:59:56 +0330

    improve meta tags

--- Components/Seo/index.tsx
+++ Components/Seo/index.tsx
@@ -21,17 +21,26 @@ function Seo({
   const { defaultTitle, defaultDescription } = siteMetadata;
   const smartTitle = titleProps || defaultTitle[locale];
   const smartDescription = descriptionProps || defaultDescription[locale];
-  const smartImage = image || `/favicon.ico`;
+  const smartImage = image || `https://marabox.com/favicon.ico`;
   return (
     <Head>
       <title>{smartTitle}</title>
+      <meta name='description' content={smartDescription} />
       <link rel='canonical' href={canonical}></link>
       <link rel='icon' href='/favicon.ico' />
-      <meta name='description' content={smartDescription} />
+      {/* <!-- Facebook Meta Tags --> */}
       <meta property='og:title' content={smartTitle} />
       <meta property='og:image' content={smartImage} />
       <meta property='og:description' content={smartDescription} />
       <meta property='og:type' content={type} />
+      <meta property='og:url' content={canonical} />
+      {/* <!-- Twitter Meta Tags --> */}
+      <meta name='twitter:card' content={smartImage} />
+      <meta property='twitter:domain' content='marabox.com' />
+      <meta property='twitter:url' content={canonical} />
+      <meta name='twitter:title' content={smartTitle} />
+      <meta name='twitter:description' content={smartDescription} />
+      <meta name='twitter:image' content={smartImage} />
     </Head>
   );
 }

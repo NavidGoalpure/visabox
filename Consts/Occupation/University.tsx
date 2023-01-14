7d7689d0365596177070764990b379e2e55ff518
navidgoalpure <navid.goalpure@gmail.com>
2023-01-14 19:59:27 +0330

    add initial structure of iran universities

---
+++ Consts/Occupation/University.tsx
@@ -0,0 +1,25 @@
+import { MultiLanguageText } from 'Interfaces';
+
+const iran: [
+  {
+    title: MultiLanguageText;
+    desc: MultiLanguageText;
+    setion: number;
+    city: MultiLanguageText;
+  }
+] = [
+  {
+    title: {
+      en: 'Abadeh Higher Education Institute',
+      fa: 'ﻣوﺳﺳﮫ آﻣوزش ﻋﺎﻟﻲ آﺑﺎده',
+    },
+    desc: {
+      en: 'Abadeh Non-profit Non-Governmental Higher Education Institute Abadeh',
+    },
+    city: {
+      en: 'Abadeh',
+      fa: 'آباده',
+    },
+    setion: 2,
+  },
+];

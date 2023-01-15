fcd2857f06a37caba7965a25e8a1ea2e6c94708b
mobin <merminator.toop@gmail.com>
2023-01-15 20:26:02 +0330

    added a few universities

--- Consts/Occupation/University.tsx
+++ Consts/Occupation/University.tsx
@@ -8,18 +8,144 @@ const iran: [
     city: MultiLanguageText;
   }
 ] = [
-  {
-    title: {
-      en: 'Abadeh Higher Education Institute',
-      fa: 'ﻣوﺳﺳﮫ آﻣوزش ﻋﺎﻟﻲ آﺑﺎده',
+    {
+      title: {
+        en: 'Abadeh Higher Education Institute',
+        fa: 'ﻣوﺳﺳﮫ آﻣوزش ﻋﺎﻟﻲ آﺑﺎده',
+      },
+      desc: {
+        en: 'Abadeh Non-profit Non-Governmental Higher Education Institute Abadeh',
+      },
+      city: {
+        en: 'Abadeh',
+        fa: 'آباده',
+      },
+      setion: 2,
     },
-    desc: {
-      en: 'Abadeh Non-profit Non-Governmental Higher Education Institute Abadeh',
+    {
+      title: {
+        en: 'Abdelrahman Sufi Higher Education Institute',
+        fa: 'موسسھ آموزش عالي عبدالرحمن صوفي',
+      },
+      desc: {
+        en: 'Abdelrahman Sufi Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: 'Zanjan',
+        fa: 'زنجان',
+      },
+      setion: 2,
     },
-    city: {
-      en: 'Abadeh',
-      fa: 'آباده',
+    {
+      title: {
+        en: 'Abdolrahman Sufi Razi Higher Education Institute',
+        fa: 'موسسھ آموزش عالي عبدالرحمن صوفي رازي',
+      },
+      desc: {
+        en: 'Abdelrahman Sufi Razi Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: 'Zanjan',
+        fa: 'زنجان',
+      },
+      setion: 2,
     },
-    setion: 2,
-  },
-];
+    {
+      title: {
+        en: 'Abyek Higher Education Institute',
+        fa: 'موسسھ آموزش عالي آبیک',
+      },
+      desc: {
+        en: 'Abyek Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: '',
+        fa: '',
+      },
+      setion: 2,
+    },
+    {
+      title: {
+        en: 'Adib Mazandaran Higher Education Institute',
+        fa: 'موسسھ آموزش عالي ادیب مازندران',
+      },
+      desc: {
+        en: 'Adib Mazandaran Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: '',
+        fa: '',
+      },
+      setion: 2,
+    },
+    {
+      title: {
+        en: 'Aerospace Research Institute',
+        fa: 'پژوھشگاه ھوا و فضا',
+      },
+      desc: {
+        en: '',
+      },
+      city: {
+        en: '',
+        fa: '',
+      },
+      setion: 2,
+    },
+    {
+      title: {
+        en: 'Afarinesh Higher Education Institute',
+        fa: 'موسسھ آموزش عالي آفرینش',
+      },
+      desc: {
+        en: 'Afarinesh Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: 'Boroujerd',
+        fa: 'بروجرد',
+      },
+      setion: 2,
+    },
+    {
+      title: {
+        en: 'Ahlul Bayt Higher Education Centre',
+        fa: 'مركز آموزش عالي اھل بیت',
+      },
+      desc: {
+        en: 'Ahlul Bayt Non-Profit Non-Governmental Higher Education Centre',
+      },
+      city: {
+        en: '',
+        fa: '',
+      },
+      setion: 2,
+    },
+    {
+      title: {
+        en: 'Ahwaz University of Medical Sciences',
+        fa: 'دانشگاه علوم پزشکی و خدمات بھداشتی درمانی اھواز',
+      },
+      desc: {
+        en: 'Ahwaz Jundishapur University of Medical Sciences Shahid Chamran University of Medical Sciences, Faculty of Medicine of Shahid Chamran University Public institution, founded in 1956'
+      },
+      city: {
+        en: 'Ahwaz',
+        fa: 'اهواز',
+      },
+      setion: 1,
+    },
+    {
+      title: {
+        en: 'Al Borz Higher Education Institute',
+        fa: 'موسسھ آموزش عالي البرز',
+      },
+      desc: {
+        en: 'Al Borz Non-profit Non-Governmental Higher Education Institute',
+      },
+      city: {
+        en: '',
+        fa: '',
+      },
+      setion: 1,
+    },
+  ];

b2b06e923841ee26d42df22a16e5d467eac0bb88
navidgoalpure <navid.goalpure@gmail.com>
2023-01-15 16:17:57 +0330

    add rest for PageErrorContent

--- Elements/Error/__test__/index.test.tsx
+++ Elements/Error/__test__/index.test.tsx
@@ -1,13 +1,16 @@
-import { screen, render, fireEvent } from 'Utils/test-utils';
 import { ComponentError } from '../componentContent';
-import mockRouter from 'next-router-mock';
+import PageErrorContent from '../pageContent';
 import renderer from 'react-test-renderer';
 jest.mock('next/router', () => require('next-router-mock'));
 
 describe('ComponentError', () => {
   /////snapshot///////
   it('renders ComponentError correctly ', () => {
-    const openComponentError = renderer.create(<ComponentError />).toJSON();
-    expect(openComponentError).toMatchSnapshot();
+    const componentError = renderer.create(<ComponentError />).toJSON();
+    expect(componentError).toMatchSnapshot();
+  });
+  it('renders ComponentError correctly ', () => {
+    const pageErrorContent = renderer.create(<PageErrorContent />).toJSON();
+    expect(pageErrorContent).toMatchSnapshot();
   });
 });

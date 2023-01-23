cbed7b9980d5cdc58f4e447fb6c899a4deca47cf
navidgoalpure <navid.goalpure@gmail.com>
2023-01-23 11:41:52 +0330

    fix bug

--- PagesComponents/Businesses/Lawyers/index.tsx
+++ PagesComponents/Businesses/Lawyers/index.tsx
@@ -1,11 +1,7 @@
-import { assssingAuthorities } from "./const";
+import { assssingAuthorities } from './const';
 
 function Content() {
   const foo = assssingAuthorities;
-  return (
-    <div>
-      <img src={foo?.[0]?.avatar} alt="" />
-    </div>
-  );
+  return <div>test</div>;
 }
 export default Content;

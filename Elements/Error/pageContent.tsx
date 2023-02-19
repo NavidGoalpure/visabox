299ebb86df6269ba386a3556a654a5496c843784
navidgoalpure <navid.goalpure@gmail.com>
2023-02-19 16:26:28 +0330

    fix bug

--- Elements/Error/pageContent.tsx
+++ Elements/Error/pageContent.tsx
@@ -71,18 +71,19 @@ const WireSvg = () => {
 export default function PageErrorContent() {
   return (
     <Container>
-      <MaraBgAnimation />
-      <TxtContainer>
-        <Wire>
-          <WireSvg />
-        </Wire>
-        <Title>Someone stepped on the wire</Title>
-        <Subtitle>Please check your connection</Subtitle>
-        <Desc>{`there most be some problems in the servers please try again later`}</Desc>
-        <Link href='/occupations'>
-          <PrimaryButton>Home</PrimaryButton>
-        </Link>
-      </TxtContainer>
+      <MaraBgAnimation>
+        <TxtContainer>
+          <Wire>
+            <WireSvg />
+          </Wire>
+          <Title>Someone stepped on the wire</Title>
+          <Subtitle>Please check your connection</Subtitle>
+          <Desc>{`there most be some problems in the servers please try again later`}</Desc>
+          <Link href='/occupations'>
+            <PrimaryButton>Home</PrimaryButton>
+          </Link>
+        </TxtContainer>
+      </MaraBgAnimation>
     </Container>
   );
 }

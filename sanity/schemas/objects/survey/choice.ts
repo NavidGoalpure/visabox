e0962626a2dcc3de5d21703754f13eb524d07069
navidgoalpure <navid.goalpure@gmail.com>
2023-02-28 14:33:29 +0330

    add sanity schema

---
+++ sanity/schemas/objects/survey/choice.ts
@@ -0,0 +1,36 @@
+// این آبجکت برای نظرسنجی های چند گزینه ای استفاده میشه
+export default {
+  name: 'survey_choice_obj',
+  title: 'Choice',
+  type: 'object',
+  fields: [
+    {
+      name: 'title_en',
+      title: 'English Choice',
+      type: 'string',
+      localize: true,
+
+      // validation: (Rule: { required: () => any }) => Rule.required(),
+    },
+    {
+      name: 'title_fa',
+      title: 'Farsi Choice',
+      type: 'string',
+      localize: true,
+
+      // validation: (Rule: { required: () => any }) => Rule.required(),
+    },
+    {
+      name: 'responders',
+      title: 'Responders',
+      type: 'array',
+      hidden: true,
+      of: [{ type: 'string' }],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'choice',
+    },
+  },
+};

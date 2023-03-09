dbca917f49e93890e6e6c97ac1c3dfef16d9937b
mobin <merminator.toop@gmail.com>
2023-03-09 15:14:15 +0330

    polished natties data and fixed subtitle in 404

--- Consts/Businesses/naati.ts
+++ Consts/Businesses/naati.ts
@@ -15,7 +15,7 @@ const naaties: Naati[] = [
             en: "Navid Armiun",
             fa: "نوید آرمیون"
         },
-        phone: "+989113410070",
+        phone: "+98 9113410070",
         email: "armiun@hotmail.com",
         language: [{
             en: "Persian to English",
@@ -43,10 +43,10 @@ const naaties: Naati[] = [
     },
     {
         fullName: {
-            en: "Seyed Hassan Amirshahi",
-            fa: "سید حسن امیرشاهی"
+            en: "Hassan Amirshahi",
+            fa: "حسن امیرشاهی"
         },
-        phone: "++98 2188311593, 98 9121077760",
+        phone: "+98 2188311593,+98 9121077760",
         email: "info@amirshahilaw.com, translation.office14@gmail.com",
         language: [{
             en: "Persian to English",
@@ -60,8 +60,8 @@ const naaties: Naati[] = [
     },
     {
         fullName: {
-            en: "Roxana Moallemian Barmi",
-            fa: "رکسانا معلمیان بارمی"
+            en: "Roxana Moallemian",
+            fa: "رکسانا معلمیان"
         },
         email: "roxanabarmi@gmail.com",
         language: [{
@@ -74,7 +74,7 @@ const naaties: Naati[] = [
             en: "Matin Amirshahi",
             fa: "متین امیرشاهی"
         },
-        phone: "+982122704817, +989013788625",
+        phone: "+98 2122704817,+98 9013788625",
         email: "translation@amirshahi.law",
         language: [{
             en: "Persian to English",
@@ -95,7 +95,7 @@ const naaties: Naati[] = [
             en: "Gholamreza Tajvidi",
             fa: "غلامرضا تجویدی"
         },
-        phone: "98 9122151862",
+        phone: "+98 9122151862",
         language: [{
             en: "Persian to English",
             fa: "فارسی به انگلیسی"
@@ -110,7 +110,7 @@ const naaties: Naati[] = [
             en: "Ali Khazaee Farid",
             fa: "علی خرایی فرید"
         },
-        phone: "00989153137087, 0098 51 38430730",
+        phone: "+98 9153137087,+98 5138430730",
         email: "rezakhfarid@gmail.com",
         language: [{
             en: "Persian to English",
@@ -126,7 +126,7 @@ const naaties: Naati[] = [
             en: "Khalil Motallebzadeh",
             fa: "خلیل مطلب زاده"
         },
-        phone: "+98(51)37672809",
+        phone: "+98 5137672809",
         email: "soroosh12@gmail.com, kmotallebz@gmail.com",
         language: [{
             en: "Persian to English",
@@ -157,7 +157,7 @@ const naaties: Naati[] = [
             en: "Mohammadreza Hosseini",
             fa: "محمدرضا حسینی"
         },
-        phone: "+98 21 66 5645 91-2, +98 9125139310",
+        phone: "+98 21665645912,+98 9125139310",
         email: "shakiba11@yahoo.com",
         language: [{
             en: "Persian to English",
@@ -178,7 +178,7 @@ const naaties: Naati[] = [
             en: "Atefeh Pashazadeh",
             fa: "عاطفه پاشازاده"
         },
-        phone: "02188822452- 02188862034, 00989211914685-9122790683",
+        phone: "+98 2188822452-+98 2188862034, +989211914685-9122790683",
         email: "atefeh.pashazadeh@gmail.com",
         language: [{
             en: "Persian to English",
@@ -199,7 +199,7 @@ const naaties: Naati[] = [
             en: "Majid Fatahipour",
             fa: "مجید فتاحی پور"
         },
-        phone: "00989122389210",
+        phone: "+98 9122389210",
         email: "majifata@gmail.com",
         language: [{
             en: "Persian to English",
@@ -215,7 +215,7 @@ const naaties: Naati[] = [
             en: "Shahram Mirzaei",
             fa: "دکتر نوید آرمیون"
         },
-        phone: "+982188240445",
+        phone: "+98 2188240445",
         email: "translation@aryagroup.com.au",
         language: [{
             en: "Persian to English",
@@ -232,7 +232,7 @@ const naaties: Naati[] = [
             en: "Alireza Mamdoohi",
             fa: "علیرضا ممدوهی"
         },
-        phone: "0912 108 3186",
+        phone: "+98 9121083186",
         email: "mamdoohi.ar@gmail.com",
         language: [{
             en: "Persian to English",
@@ -244,7 +244,7 @@ const naaties: Naati[] = [
             en: "Alireza Hassani",
             fa: "علیرضا حسنی"
         },
-        phone: "+982122743307",
+        phone: "+98 2122743307",
         email: "alirezaabiz@gmail.com",
         language: [{
             en: "Persian to English",

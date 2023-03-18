1e44c0444bc9b65a9a43e240e73a416b9b57b037
mobin <merminator.toop@gmail.com>
2023-03-18 16:41:27 +0330

    fixed natties tittle

--- PagesComponents/Boxes/Naaties/const.ts
+++ PagesComponents/Boxes/Naaties/const.ts
@@ -10,10 +10,10 @@ export enum LanguageKeys {
 export const componentStatements: Record<LanguageKeys, MultiLanguageText> = {
   SeoTitle: {
     en: 'Find the most authentic migration agents for australia| Mara Box',
-    fa: 'معتبر ترین وکلای ایرانی مهاجرت به استرالیا را پیدا کنید | مارا باکس',
+    fa: 'معتبر ترین مترجم های ایرانی مهاجرت به استرالیا را پیدا کنید | مارا باکس',
   },
   SeoDesc: {
-    fa: 'اسامی بهترین وکلای ایرانی برای اقامت و مهاجرت به استرالیا،ویزای سرمایه گذاری،کاری،خانواده و ازدواج،تجاری و تحصیلی',
+    fa: 'اسامی بهترین مترجم ایرانی برای اقامت و مهاجرت به استرالیا،ویزای سرمایه گذاری،کاری،خانواده و ازدواج،تجاری و تحصیلی',
     en: 'name and info of some of the best migration agents for migration to australia,investment visa,work visa,marriage and student visa',
   },
   //
@@ -22,8 +22,8 @@ export const componentStatements: Record<LanguageKeys, MultiLanguageText> = {
     en: 'Naati Translator',
   },
   PageDesc: {
-    fa: 'این صفحه برای افراد یا خانواده هایی است که با کمک وکیل به دنبال مهاجرت دائم به استرالیا هستند',
-    en: `This page is for people or families looking to permanently migrate to Australia with help of a agent`,
+    fa: 'این صفحه برای افراد یا خانواده هایی است که با کمک مترجم نیازمند ترجمه مدارک خود برای مهاجرت به استرالیا هستند',
+    en: `This page is for individuals or families who need the help of a translator to translate their documents to immigrate to Australia`,
   },
 };
 

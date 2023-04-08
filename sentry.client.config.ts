a466dda03a618e9e293f6d4462da70d9eeac2d68
navidgoalpure <navid.goalpure@gmail.com>
2023-04-08 17:50:33 +0330

    change hotjar with sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -2,27 +2,25 @@ import * as Sentry from '@sentry/nextjs';
 import { isItOnLive } from 'Utils';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+// بین سنتری و هاتجر یکی باید فعال باشه. با آنکامنت بودن خط های زیر سنتری فعاله
+Sentry.init({
+  dsn:
+    SENTRY_DSN ||
+    'https://f7e189da0d4b4a14bdc3691078e6ec6d@o4504577263206400.ingest.sentry.io/4504577279787008',
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+  enabled: isItOnLive(),
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 
-// navid
-// بین سنتری و هاتجر یکی باید فعال باشه. الان هاتجر فعاله
-// Sentry.init({
-//   dsn:
-//     SENTRY_DSN ||
-//     'https://f7e189da0d4b4a14bdc3691078e6ec6d@o4504577263206400.ingest.sentry.io/4504577279787008',
-//   // We recommend adjusting this value in production, or using tracesSampler
-//   // for finer control
-//   tracesSampleRate: 1.0,
-//   // ...
-//   // Note: if you want to override the automatic release value, do not set a
-//   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-//   // that it will also get attached to your source maps
-//   enabled: isItOnLive(),
-//   // This sets the sample rate to be 10%. You may want this to be 100% while
-//   // in development and sample at a lower rate in production
-//   replaysSessionSampleRate: 0.1,
-//   // If the entire session is not sampled, use the below sample rate to sample
-//   // sessions when an error occurs.
-//   replaysOnErrorSampleRate: 1.0,
-
-//   integrations: [new Sentry.Replay()],
-// });
+  integrations: [new Sentry.Replay()],
+});

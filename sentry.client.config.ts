45e26ad171d46f4d0a198c23ff4ea00f7b4e0fda
navidgoalpure <navid.goalpure@gmail.com>
2023-04-10 15:06:51 +0330

    remove replay hotjar

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -15,12 +15,4 @@ Sentry.init({
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
   enabled: isItOnLive(),
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-
-  integrations: [new Sentry.Replay()],
 });

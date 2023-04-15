36df4017b6ef4e1811392b2cfa007f765cbaa3d4
mobin <merminator.toop@gmail.com>
2023-04-15 20:49:47 +0430

    added blog

--- PagesComponents/Blog/Heading.tsx
+++ PagesComponents/Blog/Heading.tsx
@@ -1,3 +1,5 @@
+import { Layer1_TextStyle, Layer1_TitleStyle } from 'Styles/Theme/Layers/layer1/style';
+import { Headline4Style } from 'Styles/Typo';
 import React, { ReactNode } from 'react';
 import styled from 'styled-components';
 
@@ -5,5 +7,10 @@ export const HeadingH1: React.FC<{ children: ReactNode }> = ({ children }) => {
   return <H1>{children}</H1>;
 };
 const H1 = styled.h1`
-  color: red;
+${Layer1_TitleStyle}
+text-align: center !important;
 `;
+const P = styled.p`
+${Layer1_TextStyle}
+`;
+

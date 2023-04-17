fd7c515317d4e35cd304a54fc21caad9063f6504
navidgoalpure <navid.goalpure@gmail.com>
2023-04-17 17:51:08 +0330

    try to fix bug

--- pages/middleware.ts
+++ pages/middleware.ts
@@ -9,6 +9,6 @@ export function middleware(request: NextRequest) {
 }
 
 // See "Matching Paths" below to learn more
-export const config = {
-//   matcher: '/about/:path*',
-};
+// export const config = {
+// //   matcher: '/about/:path*',
+// };

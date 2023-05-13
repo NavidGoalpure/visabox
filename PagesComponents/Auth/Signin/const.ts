f3c927b1727eac82e86e7cd3b1da26594aa59687
mobin <merminator.toop@gmail.com>
2023-05-13 17:00:04 +0330

    added seo to form

--- PagesComponents/Auth/Signin/const.ts
+++ PagesComponents/Auth/Signin/const.ts
@@ -16,11 +16,11 @@ export const componentStatements: Record<LanguageKeys, MultiLanguageText> = {
     fa: "برای ورود یا ساخت اکانت از حساب کاربری دیسکورد یا گوگل خود استفاده کنید.",
   },
   SeoTitle: {
-    en: "Use your Google or Discord accounts for Signing or Register.",
-    fa: "برای ورود یا ساخت اکانت از حساب کاربری دیسکورد یا گوگل خود استفاده کنید.",
+    en: "Creating and logging in to a user account",
+    fa: "ساخت و ورود به حساب کاربری.",
   },
   SeoDesc: {
-    en: "Use your Google or Discord accounts for Signing or Register.",
-    fa: "برای ورود یا ساخت اکانت از حساب کاربری دیسکورد یا گوگل خود استفاده کنید.",
+    en: "Create an account using your virtual accounts in Marabox.",
+    fa: "با استفاده از حساب های مجازی خود در ماراباکس حساب کاربری بسازید.",
   },
 };

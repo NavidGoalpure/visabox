95536bcb2b643b57825126746568220208d4d27c
Farzam Khoddam <farzamfara85@gmail.com>
2023-05-14 12:14:31 +0330

    debug

--- pages/forms/client/index.tsx
+++ pages/forms/client/index.tsx
@@ -3,10 +3,12 @@ import PageLayout from "Components/Layouts/PageContainer";
 import { useStaticTranslation } from "Hooks/useStaticTraslation";
 import { useLocale } from "Hooks/useLocale";
 import Seo from "Components/Seo";
-import { componentStatements } from "PagesComponents/Landings/Agents/const";
 import styled from "styled-components";
 import Content from "PagesComponents/Forms/Client";
-import { LanguageKeys } from "PagesComponents/Forms/Client/const";
+import {
+  componentStatements,
+  LanguageKeys,
+} from "PagesComponents/Forms/Client/const";
 import { FormDataContextProvider } from "PagesComponents/Forms/Client/Contexts/FormDataContext/Context";
 
 const UserForms: NextPage = ({}) => {

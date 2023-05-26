8a304032fafa10692b50aefa0227a0a545845b50
navidgoalpure <navid.goalpure@gmail.com>
2023-05-26 17:51:35 +0330

    improve sitemap

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -1,13 +1,22 @@
+import { AGENCYS } from 'Consts/Lists/agency';
+import { Agents } from 'Consts/Lists/agents';
+import { FeaturedPlan_Business } from 'Interfaces/Database/Lists';
 import { Occupation } from 'Interfaces/Database/Occupation/occupation';
 import { getAllOccupationCodes } from 'Queries/sitemap';
 
+const Domain_EN = 'https://marabox.com/en';
+const Domain_FA = 'https://marabox.com/fa';
+//
 const OccupatopnsPage_EN = 'https://marabox.com/en/occupations';
 const OccupatopnsPage_FA = 'https://marabox.com/fa/occupations';
 
 function generateSiteMap(occupations: Pick<Occupation, 'slug'>[]) {
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-     <!--We manually set the two URLs we know already-->
+
+     <!------------------------------------->
+     <!--The URLs realated to Occupations-->
+     <!------------------------------------->
      <url>
        <loc>${OccupatopnsPage_EN}</loc>
      </url>
@@ -18,58 +27,90 @@ function generateSiteMap(occupations: Pick<Occupation, 'slug'>[]) {
      <url>
        <loc>${OccupatopnsPage_FA}/occupations/assssing-authorities</loc>
      </url>
-     
-     <url>
-       <loc>${OccupatopnsPage_EN}/lists/social-pages</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/social-pages</loc>
-     </url>
 
-     <url>
-       <loc>${OccupatopnsPage_EN}/lists/agencies</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/agencies</loc>
-     </url>
-
-     <url>
-       <loc>${OccupatopnsPage_EN}/lists/agencies/shada-visa</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/agencies/shada-visa</loc>
-     </url>
-
-     <url>
-       <loc>${OccupatopnsPage_EN}/lists/agents</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/agents</loc>
-     </url>
-     
-     <url>
-       <loc>${OccupatopnsPage_EN}/lists/naaties</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/naaties</loc>
-     </url>
-      
-      <url>
-       <loc>${OccupatopnsPage_EN}/lists/exchanges</loc>
-     </url>
-     <url>
-       <loc>${OccupatopnsPage_FA}/lists/exchanges</loc>
-     </url>
-     ${occupations
-       .map(({ slug }) => {
-         return `
+      ${occupations
+        .map(({ slug }) => {
+          return `
        <url>
            <loc>${`${OccupatopnsPage_EN}/${slug?.current}`}</loc>
        </url>
         
      `;
-       })
-       .join('')}
+        })
+        .join('')}
+
+     <!------------------------------------->
+     <!--The URLs realated to Lists-->
+     <!------------------------------------->
+     <url>
+       <loc>${Domain_FA}/lists/social-pages</loc>
+     </url>
+     
+
+     <url>
+       <loc>${Domain_EN}/lists/agencies</loc>
+     </url>
+     <url>
+       <loc>${Domain_FA}/lists/agencies</loc>
+     </url>
+
+     
+    ${AGENCYS.filter(
+      (agency) =>
+        agency.featuredPlan === FeaturedPlan_Business.VIP ||
+        agency.featuredPlan === FeaturedPlan_Business.FULL_DATA
+    )
+      .map(({ slug }) => {
+        return `
+       <url>
+           <loc>${`${Domain_EN}/lists/agencies/${slug}`}</loc>
+       </url>
+          <url>
+           <loc>${`${Domain_FA}/lists/agencies/${slug}`}</loc>
+       </url>
+        
+     `;
+      })
+      .join('')}
+     
+      <url>
+       <loc>${Domain_EN}/lists/agents</loc>
+     </url>
+     <url>
+       <loc>${Domain_FA}/lists/agents</loc>
+     </url>
+    ${Agents.filter(
+      (agennt) =>
+        agennt.featuredPlan === FeaturedPlan_Business.VIP ||
+        agennt.featuredPlan === FeaturedPlan_Business.FULL_DATA
+    )
+      .map(({ slug }) => {
+        return `
+       <url>
+           <loc>${`${Domain_EN}/lists/agents/${slug}`}</loc>
+       </url>
+       <url>
+           <loc>${`${Domain_FA}/lists/agents/${slug}`}</loc>
+       </url>
+        
+     `;
+      })
+      .join('')}
+          
+     <url>
+       <loc>${Domain_EN}/lists/naaties</loc>
+     </url>
+     <url>
+       <loc>${Domain_FA}/lists/naaties</loc>
+     </url>
+      
+      <url>
+       <loc>${Domain_EN}/lists/exchanges</loc>
+     </url>
+     <url>
+       <loc>${Domain_FA}/lists/exchanges</loc>
+     </url>
+    
    </urlset>
  `;
 }

e567246eef0bb3552c32ba3d5a08e12f0916b067
navidgoalpure <navid.goalpure@gmail.com>
2023-06-12 17:43:29 +1000

    try to fixx

--- Elements/ToggleGroup/Item.tsx
+++ Elements/ToggleGroup/Item.tsx
@@ -64,7 +64,7 @@ const Container = styled(ToggleGroup.Item)`
   border-radius: 1000px;
   padding: 1rem 3rem;
   width: max-content;
-  height: 1rem;
+  height: fit-content;
   transition: all 0.5s ease;
   backdrop-filter: blur(15px);
   span {

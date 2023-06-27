a55dc65dbc7bbb593a196a0ee4d22678414a138a
navidgoalpure <navid.goalpure@gmail.com>
2023-06-27 20:29:32 +1000

    improve words

--- Components/NavigationMenu/Desktop/AvatarComponent/const.ts
+++ Components/NavigationMenu/Desktop/AvatarComponent/const.ts
@@ -1,23 +1,22 @@
-import gsap from "gsap";
-import { MultiLanguageText } from "Interfaces/Database";
+import gsap from 'gsap';
+import { MultiLanguageText } from 'Interfaces/Database';
 
 export enum LanguageKeys {
-  ChangeTheme = "ChangeTheme",
-  IsBasicFormShowable = "IsBasicFormShowable",
-  LogOut = "LogOut",
+  ChangeTheme = 'ChangeTheme',
+  IsBasicFormShowable = 'IsBasicFormShowable',
+  LogOut = 'LogOut',
 }
 export const componentStatements: Record<LanguageKeys, MultiLanguageText> = {
   ChangeTheme: {
-    en: "Change Theme",
-    fa: "عوض کردن تم",
+    en: 'Change Theme',
+    fa: 'تغییر تم',
   },
   IsBasicFormShowable: {
-    // navid talk to mobin about the english text
-    en: "Is Form Sharable With Lawyers",
-    fa: "نمایش فرم به وکلای مهاجرت",
+    en: 'Share Profile With Agents?',
+    fa: 'نمایش فرم به وکلای مهاجرت',
   },
   LogOut: {
-    en: "Log out",
-    fa: "خروج از حساب کاربری",
+    en: 'Log out',
+    fa: 'خروج از حساب کاربری',
   },
 };

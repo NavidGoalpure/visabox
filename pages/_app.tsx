f4adfb078665500896b9d3ee3394c91e8981771a
navidgoalpure <navid.goalpure@gmail.com>
2023-07-19 00:43:32 +1000

    gtag with partytown

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -45,7 +45,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
           //وقتی این رو آنکامنت میکنیم  باید جیتگ/پارتیتاون رو از توی کاستوم داکیومنت کامنت کنیم
           //////////////////////////////////////////// */}
 
-      <Script id='google-tag-manager' strategy='afterInteractive'>
+      {/* <Script id='google-tag-manager' strategy='afterInteractive'>
         {`
          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -53,7 +53,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
       `}
-      </Script>
+      </Script> */}
       <NextNProgress height={2} />
 
       <SessionProvider session={session}>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -40,7 +40,7 @@ export default class MyDocument extends Document {
           /////////////GTAG With Partytown////////////
           //وقتی این رو آنکامنت میکنیم  باید جیتگ رو از توی آندرلاین اپ کامنت کنیم
           //////////////////////////////////////////// */}
-          {/* <Partytown
+          <Partytown
             debug={true}
             forward={['dataLayer.push']}
             set={(opts) => {
@@ -55,9 +55,9 @@ export default class MyDocument extends Document {
               }
               return opts.continue;
             }}
-          /> */}
+          />
 
-          {/* <script
+          <script
             defer
             type='text/partytown'
             dangerouslySetInnerHTML={{
@@ -69,18 +69,18 @@ export default class MyDocument extends Document {
         })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
       `,
             }}
-          /> */}
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
-          {/* {isItOnLive() && (
+          {isItOnLive() && (
             <noscript
               dangerouslySetInnerHTML={{
                 __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}" height="0" width="0" style="display: none; visibility: hidden;" />`,
               }}
             />
-          )} */}
+          )}
         </body>
       </Html>
     );

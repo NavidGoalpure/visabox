2822e608d2976cd53594ddc58e9ba8a4da108df5
Farzam Khoddam <farzamfara85@gmail.com>
2023-08-01 12:45:59 +0330

    debug hint

--- Styles/Theme/Hint/style.ts
+++ Styles/Theme/Hint/style.ts
@@ -1,12 +1,18 @@
 import { css } from "styled-components";
-import { Headline4Style, Headline5Style, Headline6Style, Headline7Style } from "Styles/Typo";
+import {
+  Headline4Style,
+  Headline5Style,
+  Headline6Style,
+  Headline7Style,
+} from "Styles/Typo";
 import { directionStyles } from "..";
-import { SecondaryTextTheme, IconTheme } from "./theme";
+import { SecondaryTextTheme, IconTheme, Hint_BG } from "./theme";
 
 export const Hint_TitleStyle = css`
-  ${directionStyles};
   ${Headline4Style};
-  color: white;
+  ${directionStyles};
+  color: var(--color-secondary4) !important;
+  text-align: center !important;
 `;
 export const Hint_SubTitleStyle = css`
   ${directionStyles};
@@ -17,6 +23,11 @@ export const Hint_TextStyle = css`
   ${directionStyles};
   ${Headline6Style};
   color: white;
+`;
+export const Hint_BGStyle = css`
+  ${Hint_BG}
+  border-radius: 15px;
+  padding: 2rem 4rem;
 `;
 export const Hint_SecondaryContainer = css`
   display: flex;

0c6d46e4cfa014e388cd721152484a45c712afb3
Farzam Khoddam <farzamfara85@gmail.com>
2023-08-17 15:51:10 +0330

    bugfix: list/agents was not showing all the lawyes

--- PagesComponents/Lists/Agents/utils.ts
+++ PagesComponents/Lists/Agents/utils.ts
@@ -1,27 +1,49 @@
-import { Locations } from 'Interfaces';
-import { FeaturedPlan_Business } from 'Interfaces/Database/Lists';
-import { Agent } from 'Interfaces/Database/Lists/agents';
+import { useLocale } from "Hooks/useLocale";
+import { Languages, Locations } from "Interfaces";
+import { FeaturedPlan_Business } from "Interfaces/Database/Lists";
+import { Agent } from "Interfaces/Database/Lists/agents";
 
 export function getSmartVIPAgent(agents: Agent[]): Agent[] {
+  const { locale } = useLocale();
   const result = agents.filter((agent) => {
-    if (
-      (agent.featuredPlan === FeaturedPlan_Business.VIP ||
-        agent.featuredPlan === FeaturedPlan_Business.FULL_DATA) &&
-      agent.locations.includes(Locations.fa)
-    ) {
-      return agent;
+    if (locale === Languages.fa) {
+      if (
+        (agent.featuredPlan === FeaturedPlan_Business.VIP ||
+          agent.featuredPlan === FeaturedPlan_Business.FULL_DATA) &&
+        agent.locations.includes(Locations.fa)
+      ) {
+        return agent;
+      }
+    } else if (locale === Languages.en) {
+      if (
+        (agent.featuredPlan === FeaturedPlan_Business.VIP ||
+          agent.featuredPlan === FeaturedPlan_Business.FULL_DATA) &&
+        agent.locations.includes(Locations.en)
+      ) {
+        return agent;
+      }
     }
   });
   return result;
 }
 
 export function getSmartSimpleAgent(agents: Agent[]): Agent[] {
+  const { locale } = useLocale();
   const result = agents.filter((agent) => {
-    if (
-      agent.featuredPlan === FeaturedPlan_Business.SIMPLE &&
-      agent.locations.includes(Locations.fa)
-    ) {
-      return agent;
+    if (locale === Languages.fa) {
+      if (
+        agent.featuredPlan === FeaturedPlan_Business.SIMPLE &&
+        agent.locations.includes(Locations.fa)
+      ) {
+        return agent;
+      }
+    } else if (locale === Languages.en) {
+      if (
+        agent.featuredPlan === FeaturedPlan_Business.SIMPLE &&
+        agent.locations.includes(Locations.en)
+      ) {
+        return agent;
+      }
     }
   });
   return result;

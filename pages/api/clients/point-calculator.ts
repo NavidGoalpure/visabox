30e4fd838731045ae5e84983459a78a5576db94d
Farzam Khoddam <farzamfara85@gmail.com>
2023-09-18 12:53:07 +0330

    debug

--- pages/api/clients/point-calculator.ts
+++ pages/api/clients/point-calculator.ts
@@ -9,17 +9,17 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     sanityClient
       // uncomment only if you want to delete your account from sanity
       // do not push this uncommented to main branch under any circumstance
-      .delete({ query: `*[_type =="client" && _id == '${client?._id}']` })
-      // .patch(client?._id)
-      // .set(client)
-      // .commit()
-      // .then(() => {
-      //   res.status(200).json({ message: "success" });
-      // })
-      // .catch((err) => {
-      //   const errors = err?.response?.body?.error?.items;
-      //   res.status(500).send({ message: "request failed", errors });
-      // });
+      // .delete({ query: `*[_type =="client" && _id == '${client?._id}']` })
+      .patch(client?._id)
+      .set(client)
+      .commit()
+      .then(() => {
+        res.status(200).json({ message: "success" });
+      })
+      .catch((err) => {
+        const errors = err?.response?.body?.error?.items;
+        res.status(500).send({ message: "request failed", errors });
+      });
   } else {
     res.status(401).send({
       message: "The user has not _id",

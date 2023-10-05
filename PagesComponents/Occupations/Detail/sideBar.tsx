7dff033995617de5264ba598ce2a3b18ceb08dc6
navidgoalpure <navid.goalpure@gmail.com>
2023-10-05 16:33:08 +1100

    fix date bug

--- PagesComponents/Occupations/Detail/sideBar.tsx
+++ PagesComponents/Occupations/Detail/sideBar.tsx
@@ -113,7 +113,7 @@ const SidebarPage: React.FC<Props> = ({ occupation }) => {
                           <HintItem>
                             <Tag>
                               {t(LanguageKeys.LastUpdate)}
-                              <span> 04 October 2023 </span>
+                              <span> 30/09/2023 </span>
                             </Tag>
                           </HintItem>
                         </AccordionContentContainer>

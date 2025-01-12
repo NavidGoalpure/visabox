1037b7d6b66bcf88cf32c8c4582730536f71fa9d
farzamkhoddam <farzamfara85@gmail.com>
2025-01-12 16:56:31 +0330

    fix homepage occupation section not having any tasks data

--- PagesComponents/Home/OccupationSection/const.ts
+++ PagesComponents/Home/OccupationSection/const.ts
@@ -25,11 +25,23 @@ export const componentStatements: Record<LanguageKeys, MultiLanguageText> = {
 export const CardData = {
   code: 133111,
   title: {
-    en: 'Construction Project Manager',
+    en: "Construction Project Manager",
   } as MultiLanguageText,
   description: {
-    en: 'Plans, organises, directs, controls and coordinates construction of civil engineering and building projects, and the physical and human resources involved in the construction process. Registration or licensing may be required.',
+    en: "Plans, organises, directs, controls and coordinates construction of civil engineering and building projects, and the physical and human resources involved in the construction process. Registration or licensing may be required.",
   } as MultiLanguageText,
-  slug: { current: 'construction-project-manager' } as Slug,
-  tasks: {} as MultiLanguageTextArray,
+  slug: { current: "construction-project-manager" } as Slug,
+  tasks: {en:[
+    "Interpreting architectural drawings and specifications",
+    "Coordinating labour resources, and procurement and delivery of materials, plant and equipment",
+    "Consulting with Architects, Engineering Professionals and other professionals, and Technical and Trades Workers",
+    "Negotiating with building owners, property developers and subcontractors involved in the construction process to ensure projects are completed on time and within budget",
+    "Preparing tenders and contract bids",
+    "Operating and implementing coordinated work programs for sites",
+    "Ensuring adherence to building legislation and standards of performance, quality, cost and safety",
+    "Arranging submission of plans to local authorities",
+    "Building under contract, or subcontracting specialised building services",
+    "Overseeing the standard and progress of subcontractors work",
+    "Arranging building inspections by local authorities",
+  ]},
 };
